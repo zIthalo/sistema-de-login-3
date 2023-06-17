1900581a1a66ed959cc2bba8a271d0201132b9ca
zIthalo <ithalowillian@gmail.com>
2023-06-17 20:24:17 -0300

    update

--- ia.php
+++ ia.php
@@ -20,23 +20,10 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
     <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
 
 
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/ia.css">
 </head>
 
 <body>
-
-    <!-- <div id="log">
-
-        <p id="l1"><a href="login.php" class="btn-log"><strong>Entre</strong></a></p>
-
-        <p id="l2"><a href="formulario.php" class="btn-log"><strong>Cadastre-se</strong></a></p>
-
-    </div> -->
-    <!-- <header id="cabeçalho">
-        <h1>Curiosidades de tecnologia</h1>
-        <p>Tudo aquilo que você sempre quis saber sobre o mundo Tech em um único lugar</p>
-    </header> -->
-
     <nav class="navbar navbar-expand-lg bg-body-tertiary colorNav">
         <div class="container-fluid">
             <a class="navbar-brand linkNav" href="#">8-BITS</a>
@@ -56,7 +43,7 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
                             Curiosidades
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="ia.php">Inteligência Artificial</a></li>
+                            <li><a class="dropdown-item" href="index.php">Android</a></li>
                             <li><a class="dropdown-item" href="#"></a></li>
                             <li>
                         </ul>
@@ -71,11 +58,6 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
                             <li>
                         </ul>
                 </ul>
-                <!-- <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form> -->
-
                 <div class="form-inline my-2 my-lg-0">
                     <label class="mr-5" for="">
                         <?php echo "Olá, ".$nomeUser."!    ";#esse código foi criado para mostrar o nome do usuário que estiver logado no sistema aqui na barra de menu do meu site?></label>
@@ -87,126 +69,82 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
     <main>
         <article>
             <!--Shift + tab para voltar a indentação-->
-            <h1>História do Mascote do Android</h1>
+            <h1>O que é a Inteligência Artificial?</h1>
 
-            <p>Provavelmente você sabe que o sistema operacional <strong>Android</strong>, mantido pelo
-                <strong>Google</strong> é um dos mais utilizados para dispositivos móveis em todo o mundo. Mas tavez
-                você não saiba que o seu simpático mascote tem um nome e uma história muito curiosa? Pois acompanhe esse
-                artigo para aprender muita coisa sobre esse robozinho.
+            <p>A Inteligência Artificial <strong>(IA)</strong> é um campo da ciência cujo propósito é estudar,
+                desenvolver e empregar máquinas para realizarem atividades humanas de maneira autônoma. </p>
+
+            <p>A <strong>IA</strong> envolve o agrupamento de várias tecnologias, como redes neurais artificiais,
+                algoritmos, sistemas de aprendizado, entre outros que conseguem simular capacidades humanas ligadas ao
+                aprendizado, solução de problemas, compreensão da linguagem e tomada de decisões.</p>
+
+            <p>Também está ligada à robótica, ao Machine Learning (Aprendizagem de Máquina), ao reconhecimento de voz e
+                de visão, entre outras tecnologias.</p>
+
+            <p>Com maior desenvolvimento a partir da década de 1950, a inteligência artificial já faz parte da vida
+                cotidiana das pessoas por meio dos assistentes de voz, dos mecanismos de pesquisa, dos carros autônomos
+                e das redes sociais.</p>
+
+            <p>Apesar de trazerem inúmeros benefícios e avanços importantes em diversas áreas, muito se debate a
+                respeito dos limites éticos da inteligência artificial e do papel que elas desempenham na nossa
+                sociedade atual.</p>
+
+
+
+            <h2>Como funciona a inteligência artificial?
+            </h2>
+
+            <p>O funcionamento da inteligência artificial, de maneira simplificada, acontece por meio da coleta e da
+                combinação de um grande volume de dados seguido da identificação de determinados padrões nesse conjunto
+                de informações. Com esse processo, que geralmente se dá mediante a utilização de algoritmos
+                pré-programados, o software consegue tomar decisões e realizar tarefas de maneira autônoma.
             </p>
 
-            <h2>A primeira versão</h2>
-
-            <p>A primeira tentativa de criar um mascote surgiu em 2007 e veio de um desenvolvedor chamado <a
-                    class="externo"
-                    href="https://androidcommunity.com/dan-morrill-shows-us-the-android-mascot-that-almost-was-20130103/"
-                    target="_blank" rel="external">Dan Morrill</a>. Ele conta que abriu o <a class="externo"
-                    href="https://inkscape.org/pt-br/" target="_blank">Inkscape</a> (software livre para vetorização de
-                imagens) e criou sua própria versão de robô. O objetivo era apenas personificar o sistema apenas para a
-                a sua equipe, não existia nenhuma solicitação da empresa para a criação de um mascote.</p>
-
-            <picture>
-                <source media="(max-width: 668px)" srcset="imagens/dan-droids-pq.png" type="image/png">
-                <img src="img/dan-droids.png" alt="primeiro mascote do android">
+            <picture class="ia">
+                <source class="ia" media="(max-width: 668px)" srcset="img/ia-pq.jpg" type="image/jpg">
+                <img src="img/ia.jpg" alt="imagem inteligência artificial">
             </picture>
 
-            <p>Essa primeira versão bizarra até foi batizada em homenagem ao seu criador: seriam os
-                <strong>Dandroids</strong>.
-            </p>
 
-            <h2>Surge um novo mascote</h2>
 
-            <p>A ideia de ter um mascote foi amadurecendo e a missão foi passada para uma profissional da área. A
-                ilustradora Russa <a class="externo" href="https://www.irinablok.com/android" target="_blank">Irina
-                    Blok</a>, também funcionária do Google, ficou com a missão de representar o pequeno robô de uma
-                maneira mais agradável.</p>
+            <h2 class="titulo2">Inteligência Artificial no Dia a Dia
+            </h2>
 
-            <picture>
-                <source class="irn" media="(max-width: 668px)" srcset="imagens/irina-blok-pq.jpg" type="image/jpg">
-                <img class="irn" src="img/irina-blok.jpg" alt="irina blok criadora do Bugdroid">
-            </picture>
+            <p>A evolução tecnológica vivenciada pela humanidade nas últimas décadas proporcionou a incorporação cada
+                vez maior da inteligência artificial na vida cotidiana. Veja, a seguir, alguns exemplos de como
+                diferentes modelos de IA se fazem presentes.</p>
 
-            <p>A ideia principal da Irina era representar tudo graficamente com poucos traços e de forma mais chapada. O
-                desenho também deveria gerar identificação rápida com quem o olha. Surgiu então o
-                <strong>Bugdroid</strong>, o novo mascote do Android.
-            </p>
 
-            <img class="bug" src="img/bugdroid.png" alt="bugdroid">
+            <p><strong>Assistentes de voz:</strong> presentes em celulares e dispositivos como caixas de som
+                inteligentes (smart speakers), os assistentes são um modelo de IA que reconhece e executa comandos
+                feitos por meio da voz, como realizar ligações, programar alarmes, dar informações, tocar música e fazer
+                pesquisas online. Os mais famosos assistentes de voz são o Google Assistente (integrado ao sistema
+                Android); Siri, da Apple; e Alexa, da Amazon.</p>
 
-            <p>A principal inspiração para os traços do novo <strong>Bugdroid</strong> veio daqueles bonequinhos que
-                ilustram portas de banheiro para indicar o gênero de cada porta. Conta a lenda que a artista estava
-                criando em sua mesa no escritório do <strong>Google</strong> e olhou para o lado dos banheiros e a
-                identificação foi imediata: simples, limpo, objetivo.</p>
+            <p><strong>Reconhecimento facial:</strong> empregada na confirmação da identidade de uma pessoa ao acessar
+                seus dispositivos pessoais, como smartphones, ou ainda em aplicativos financeiros, como os bancos
+                virtuais. Mais recentemente, o reconhecimento facial tem sido aperfeiçoado para a sua adoção ampla em
+                sistemas de segurança pública.</p>
+
+            <p><strong>Redes sociais:</strong> os conteúdos das redes sociais que utilizamos todos os dias, como
+                Instagram, Twitter e Facebook, não são apresentados da mesma maneira para todos os usuários. Isso porque
+                existe um algoritmo que analisa os padrões da atividade dos indivíduos e personaliza qual tipo de
+                postagem aparecerá para cada um, de acordo com os seus interesses.</p>
+
+
+
 
             <div class="video">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/l2UDgpLz20M"
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/UhA_ZgI-otM"
                     title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowfullscreen></iframe>
             </div>
 
-            <aside>
-                <h3>Quer aprender mais?</h3>
-
-                <p>Outro assunto curioso em relação ao <strong>Android</strong> é que cada versão sempre foi nomeada em
-                    homenagem a um doce, em ordem alfabética a partir da versão 1.5 até a 9.0.</p>
-
-                <ul id="lista-android">
-                    <li class="item-droid">
-                        1.5 - <abbr title="Bolo de bacia">Cupcake</abbr>
-                    </li>
-                    <li class="item-droid">
-                        1.6 - <abbr title="Rosquinha">Donut</abbr>
-                    </li>
-                    <li class="item-droid">
-                        3.0 - <abbr title="Bomba">Eclair</abbr>
-                    </li>
-                    <li class="item-droid">
-                        2.2 - <abbr title="Iogurte congelado">Froyo</abbr>
-                    </li>
-                    <li class="item-droid">
-                        2.3 - <abbr title="Biscoito de gengibre">Gingerbread</abbr>
-                    </li>
-                    <li class="item-droid">
-                        3.0 - <abbr title="Favo de mel">Honeycomb</abbr>
-                    </li>
-                    <li class="item-droid">
-                        4.0 - <abbr title="Sanduiche de sorvete">Ice Cream Sandwich</abbr>
-                    </li>
-                    <li class="item-droid">
-                        4.1 - <abbr title="Jujuba">Jelly Bean</abbr>
-                    </li>
-                    <li class="item-droid">
-                        4.4 - <abbr title="kitkat">KitKat</abbr>
-                    </li>
-                    <li class="item-droid">
-                        5.0 - <abbr title="Pirulito">Lolipop</abbr>
-                    </li>
-                    <li class="item-droid">
-                        6.0 - <abbr title="Marshmallow">Marshmallow</abbr>
-                    </li>
-                    <li class="item-droid">
-                        7.0 - <abbr title="Torrone">Nougat</abbr>
-                    </li>
-                    <li class="item-droid">
-                        8.0 - <abbr title="Biscoito Oreo">Oreo</abbr>
-                    </li>
-                    <li class="item-droid">
-                        9.0 - <abbr title="Torta">Pie</abbr>
-                    </li>
-                </ul>
-                <p>
-                    Infelizmente, o <strong>Android Q</strong> não existiu, pois o Google resolveu pôr fim a essa
-                    divertida prática e começou a usar numerações, o que deu origem ao Android 10.
-                    Acesse aqui o site <a class="externo" href="https://www.android.com/intl/pt_pt/history/"
-                        target="_blank">Android History</a> para conhecer a sequência das versões "adocicadas" e o que
-                    cada uma trouxe para o sistema Android.
-                </p>
-            </aside>
 
 
-            <p>Então é isso! Espero que você tenha gostado do nosso artigo com essa curiosidade sobre o sistema Android
-                e seu simpático mascote.</p>
+            <p>Então é isso! Espero que você tenha gostado do nosso artigo com essa curiosidade sobre a inteligência
+                artificial.</p>
             <p><strong>Este site foi apresentado aqui:</strong></p>
             <p class="map"><iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.405129022287!2d-34.9107968856167!3d-7.8525986943421175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab14d50f29c135%3A0xb7c10c823a37123b!2sEscola%20T%C3%A9cnica%20Estadual%20Jurandir%20Bezerra%20Lins!5e0!3m2!1spt-BR!2sbr!4v1669729072171!5m2!1spt-BR!2sbr"
@@ -345,15 +283,7 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
             <div class="swiper-pagination"></div>
         </div>
 
-        <!-- <p class="dev"> <a href="https://github.com/zIthalo" target="_blank">Ithalo Willian</a></p>
-        
-            <p class="dev"><a href="https://github.com/Alyssonx18" target="_blank">Alysson Roberto</a></p>
-    
-            <p class="dev"><a href="https://github.com/Matheushennrique" target="_blank">Matheus Henrique</a></p>
-    
-            <p class="dev"><a href="#">Tarciano Cruz</a></p>
-        
-            <p class="dev"><a href="https://instagram.com/nicolas_lima04?igshid=ZmRlMzRkMDU=" target="_blank">Nicolas Lima</a></p> -->
+
     </footer>
     <script src="./script/swiper-bundle.min.js"></script>
 
