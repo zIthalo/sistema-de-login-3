791542be57e8e1b4c75def1dc80f349457849a27
zIthalo <ithalowillian@gmail.com>
2023-06-19 18:09:04 -0300

    mudando nomes nunca utilizados

--- gpt.php
+++ gpt.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['id']) && !empty($_SESSION['id'])):// Se a sessão com o id e
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Como surgiu o mascote do android?</title>
+    <title>GPT</title>
 
     <link rel="stylesheet" href="style/swiper-bundle.min.css">
 
