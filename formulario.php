0ed2ad0d1a3f5bda72632acc416bc47ebcf3de06
zIthalo <ithalowillian@gmail.com>
2023-06-19 11:15:06 -0300

    update

--- formulario.php
+++ formulario.php
@@ -1,23 +1,8 @@
 <?php
     //esse if serve para verificar se minhas infos foram submitadas para o meu DB
-    if(isset($_POST['submit'])){ //is set* se existir a variável submit faça
-        // print_r('Nome: ' . $_POST['nome']);
-        // print_r('<br>');
-        // print_r('Email: ' . $_POST['email']);
-        // print_r('<br>');
-        // print_r('Tel: ' . $_POST['telefone']);
-        // print_r('<br>');
-        // print_r('Sexo: ' . $_POST['genero']);
-        // print_r('<br>');
-        // print_r('Nascimento: ' . $_POST['dataNasc']);
-        // print_r('<br>');
-        // print_r('Cidade: ' . $_POST['cidade']);
-        // print_r('<br>');
-        // print_r('Estado: ' . $_POST['estado']);
-        // print_r('<br>');
-        // print_r('Endereço: ' . $_POST['endereco']);
-        //Cada um destes prints r serve para mostrar os conteúdos que eu inseri em cada variável de acordo com meu formulário. Para ficar mais claro é assim: escreva(postando['o conteúdo inserido no campo name em html'])
-        include ('conexao1.php'); //aqui eu incluo minhas configs do php
+    if(isset($_POST['submit'])){
+        include 'conexao1.php'; //chame a conexão dois.php
+        
         $nome = $_POST['nome'];
         $email = $_POST['email'];
         $senha = $_POST['senha'];
@@ -31,7 +16,7 @@
         //  //A diferença do código acima para este aqui é que neste trecho eu tô pegando as informações de cada name em html através do $_POST['nome'], por exemplo e colocando-as dentro de uma variável ali acima tendeu?
 
         //Agora vou fazer minhas queries ↓↓
-        $result = mysqli_query($conexao, "INSERT INTO usuarios(nome, email,senha,telefone,sexo,datanasc,cidade,estado,endereco) VALUES ('{$nome}', '{$email}','{$senha}', '{$telefone}','{$sexo}','{$dataNasc}','{$cidade}','{$estado}','{$endereco}')");// antes, nesse código, eu tinha feito os nomes das variáveis sem as aspas e as chaves. Deu errado. Veja o config.php para saber outros erros
+        $result = mysqli_query($conexao, "INSERT INTO usuarios(nome, email, senha, telefone, sexo,datanasc, cidade, estado, endereco) VALUES ('{$nome}', '{$email}', '{$senha}', '{$telefone}', '{$sexo}', '{$dataNasc}', '{$cidade}','{$estado}', '{$endereco}')");// antes, nesse código, eu tinha feito os nomes das variáveis sem as aspas e as chaves. Deu errado. Veja o config.php para saber outros erros
 
         header('Location: login.php');
     } 
@@ -61,20 +46,29 @@
                 <br>
                 <section class="inputBox">
                     <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Nome Completo</label>
+                    <label for="nome" class="labelInput">Nome</label>
+
                 </section>
+
                 <section class="inputBox">
+
                     <input type="text" name="email" id="email" class="inputUser" required>
                     <label for="email" class="labelInput">Email</label>
+
                 </section>
+
                 <section class="inputBox">
+
                     <input type="password" name="senha" id="senha" class="inputUser" required>
                     <label for="senha" class="labelInput">Senha</label>
+
                 </section>
+
                 <section class="inputBox">
                     <input type="tel" name="telefone" id="telefone" class="inputUser" required>
                     <label for="telefone" class="labelInput">Telefone</label>
                 </section>
+
                 <p>Sexo</p>
                 <input type="radio" id="feminino" name="sexo" value="feminino" required>
                 <label for="feminino">Feminino</label>
@@ -82,26 +76,44 @@
 
                 <input type="radio" id="masculino" name="sexo" value="masculino" required>
                 <label for="masculino">Masculino</label>
+
                 <br>
+
                 <input type="radio" id="outro" name="sexo" value="outro" required>
                 <label for="outro">Outro</label>
+
                 <br>
-                <label for="dataNasc"><strong>Data de Nascimento:</strong></label>
-                <input type="date" name="dataNasc" id="dataNasc" required>
+
+                <label for="datanasc"><strong>Data de Nascimento:</strong></label>
+
+                <input type="date" name="datanasc" id="datanasc" required>
 
                 <section class="inputBox">
+
                     <input type="text" name="cidade" id="cidade" class="inputUser" required>
+
                     <label for="cidade" class="labelInput">Cidade</label>
+
                 </section>
+
                 <section class="inputBox">
+
                     <input type="text" name="estado" id="estado" class="inputUser" required>
+
                     <label for="estado" class="labelInput">Estado</label>
+
                 </section>
+
                 <section class="inputBox">
+
                     <input type="text" name="endereco" id="endereco" class="inputUser" required>
+
                     <label for="endereco" class="labelInput">Endereço</label>
+
                 </section>
+
                 <input type="submit" name="submit" id="submit">
+
             </fieldset>
 
         </form>
